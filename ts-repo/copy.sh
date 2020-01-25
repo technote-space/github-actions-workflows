b18b895a2bd02133842863fc873f1bcebab33418
Technote <technote.space@gmail.com>
2020-01-26 01:30:43 +0900

    chore: tweaks

--- ts-repo/copy.sh
+++ ts-repo/copy.sh
@@ -19,6 +19,6 @@ cp -f "${common}"/workflows/*.yml "${GITHUB_WORKSPACE}"/.github/workflows/
 cp -f "${common}"/settings/*.yml "${GITHUB_WORKSPACE}"/.github/
 cp -f "${current}"/*.yml "${GITHUB_WORKSPACE}"/.github/workflows/
 cp -f "${tmp}"/sync-workflows.yml "${GITHUB_WORKSPACE}"/.github/workflows/sync-workflows.yml > /dev/null 2>&1 || :
-cp -f "${current}"/release/"${1}"/.yml "${GITHUB_WORKSPACE}"/.github/workflows/release.yml
+cp -f "${current}"/release/"${1}".yml "${GITHUB_WORKSPACE}"/.github/workflows/release.yml
 
 rm -rdf "${tmp}"
