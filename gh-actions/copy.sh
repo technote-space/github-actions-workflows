33361dcbaadbd9f37a5f834004cde5a1ce85f40e
Technote <technote.space@gmail.com>
2020-02-09 00:52:57 +0900

    feat: update ci workflow

--- gh-actions/copy.sh
+++ gh-actions/copy.sh
@@ -15,10 +15,6 @@ cp -f "${common}"/settings/*.yml "${GITHUB_WORKSPACE}"/.github/
 cp -f "${current}"/*.yml "${GITHUB_WORKSPACE}"/.github/workflows/
 cp -f "${tmp}"/sync-workflows.yml "${GITHUB_WORKSPACE}"/.github/workflows/sync-workflows.yml > /dev/null 2>&1 || :
 
-if [ 0 = "$(< "${GITHUB_WORKSPACE}"/package.json jq -r '. | keys' | grep -c files)" ]; then
-	sed -i "s/target: \['npm', 'gpr'\]/target: ['npm']/" .github/workflows/release.yml
-fi
-
 # shellcheck disable=SC2039
 find "${GITHUB_WORKSPACE}"/.github/workflows -maxdepth 1 -type f -name '*.yml' -print0 | xargs -0 readlink -f | while read -r file; do sed -i "s/uses: \(${GITHUB_REPOSITORY/\//\\/}\)@.\+$/uses: \1@gh-actions/" "$file"; done
 rm -rdf "${tmp}"
