3b44afb0c6639c647438ee2daca240d38345f191
Technote <technote.space@gmail.com>
2020-02-11 00:46:39 +0900

    feat: update

--- js-repo/copy.sh
+++ js-repo/copy.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ $# -lt 1 ]; then
-  echo "usage: $0 <release type> [ci type = test]"
+  echo "usage: $0 <release type>"
   exit 1
 fi
 
@@ -19,7 +19,7 @@ cp -f "${common}"/workflows/*.yml "${GITHUB_WORKSPACE}"/.github/workflows/
 cp -f "${common}"/settings/*.yml "${GITHUB_WORKSPACE}"/.github/
 cp -f "${current}"/*.yml "${GITHUB_WORKSPACE}"/.github/workflows/
 cp -f "${tmp}"/sync-workflows.yml "${GITHUB_WORKSPACE}"/.github/workflows/sync-workflows.yml > /dev/null 2>&1 || :
-cp -f "${current}"/release/"${1}".yml "${GITHUB_WORKSPACE}"/.github/workflows/release.yml
-cp -f "${current}"/ci/"${2:-test}".yml "${GITHUB_WORKSPACE}"/.github/workflows/ci.yml
+cp -f "${current}"/ci/"${1}".yml "${GITHUB_WORKSPACE}"/.github/workflows/ci.yml
+rm -f "${GITHUB_WORKSPACE}"/.github/workflows/gh-releases.yml
 
 rm -rdf "${tmp}"
--- ts-repo/copy.sh
+++ ts-repo/copy.sh
@@ -3,11 +3,11 @@
 set -e
 
 if [ $# -lt 1 ]; then
-  echo "usage: $0 <release type> [ci type = test]"
+  echo "usage: $0 <release type>"
   exit 1
 fi
 
-readonly current=$(cd "$(dirname "$0")"; pwd)
+readonly current=$(cd "$(dirname "$0")"/../js-repo; pwd)
 readonly common=$(cd "$(dirname "$0")"/../common; pwd)
 readonly tmp="${current}"/.tmp
 
@@ -19,7 +19,6 @@ cp -f "${common}"/workflows/*.yml "${GITHUB_WORKSPACE}"/.github/workflows/
 cp -f "${common}"/settings/*.yml "${GITHUB_WORKSPACE}"/.github/
 cp -f "${current}"/*.yml "${GITHUB_WORKSPACE}"/.github/workflows/
 cp -f "${tmp}"/sync-workflows.yml "${GITHUB_WORKSPACE}"/.github/workflows/sync-workflows.yml > /dev/null 2>&1 || :
-cp -f "${current}"/release/"${1}".yml "${GITHUB_WORKSPACE}"/.github/workflows/release.yml
-cp -f "${current}"/ci/"${2:-test}".yml "${GITHUB_WORKSPACE}"/.github/workflows/ci.yml
+cp -f "${current}"/ci/"${1}".yml "${GITHUB_WORKSPACE}"/.github/workflows/ci.yml
 
 rm -rdf "${tmp}"
