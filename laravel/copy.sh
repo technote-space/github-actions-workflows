4da4b318f9b5e2b1c0446dfcfc077113a1561fa3
Technote <technote.space@gmail.com>
2020-02-12 21:57:54 +0900

    feat: for laravel project

---
+++ laravel/copy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+readonly current=$(cd "$(dirname "$0")"; pwd)
+readonly common=$(cd "$(dirname "$0")"/../common; pwd)
+readonly tmp="${current}"/.tmp
+
+mkdir -p "${tmp}"
+cp -f "${GITHUB_WORKSPACE}"/.github/workflows/sync-workflows.yml "${tmp}"/ > /dev/null 2>&1 || :
+rm -f "${GITHUB_WORKSPACE}"/.github/workflows/*.yml
+
+cp -f "${common}"/workflows/*.yml "${GITHUB_WORKSPACE}"/.github/workflows/
+cp -f "${common}"/settings/*.yml "${GITHUB_WORKSPACE}"/.github/
+cp -f "${current}"/*.yml "${GITHUB_WORKSPACE}"/.github/workflows/
+cp -f "${tmp}"/sync-workflows.yml "${GITHUB_WORKSPACE}"/.github/workflows/sync-workflows.yml > /dev/null 2>&1 || :
+rm -f "${GITHUB_WORKSPACE}"/.github/workflows/check-version.yml
+rm -f "${GITHUB_WORKSPACE}"/.github/workflows/gh-releases.yml
+
+rm -rdf "${tmp}"
