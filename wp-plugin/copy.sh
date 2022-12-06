def3742bf8a1eb421e0d77a3eaf53e85ce716a39
Technote <technote.space@gmail.com>
2022-12-07 02:16:59 +0900

    chore: tweaks

--- wp-plugin/copy.sh
+++ wp-plugin/copy.sh
@@ -18,12 +18,4 @@ cp -f "${current}"/*.yml "${GITHUB_WORKSPACE}"/.github/workflows/
 cp -f "${tmp}"/sync-workflows.yml "${GITHUB_WORKSPACE}"/.github/workflows/sync-workflows.yml > /dev/null 2>&1 || :
 rm -f "${GITHUB_WORKSPACE}"/.github/workflows/gh-releases.yml
 
-# shellcheck disable=SC2016
-MINUTE=$(echo "${GITHUB_REPOSITORY}" | md5sum | tr -d -c 0-9 | xargs -I{} echo 1{}12345678 | cut -c 1-8 | xargs -I{} bash -c 'echo $(({} % 60))')
-# shellcheck disable=SC2016
-HOUR=$(echo "${GITHUB_REPOSITORY}" | md5sum | tr -d -c 0-9 | xargs -I{} echo 1{}12345678 | cut -c 1-8 | xargs -I{} bash -c 'echo $(({} % 24))')
-# shellcheck disable=SC2016
-DATE=$(echo "${GITHUB_REPOSITORY}" | md5sum | tr -d -c 0-9 | xargs -I{} echo 1{}12345678 | cut -c 1-8 | xargs -I{} bash -c 'echo $(({} % 28 + 1))')
-sed -i "s/cron:.\+$/cron: ${MINUTE} ${HOUR} ${DATE} * */" "${GITHUB_WORKSPACE}"/.github/workflows/broken-link-check.yml
-
 rm -rdf "${tmp}"
